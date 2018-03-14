9eb3d184f6e8174d75e4560390ac33e227f55a38
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-03-14 21:41:58 +0900

    Enable cdv-loader with webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,7 +74,16 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
+      {
+        test: /\.csv$/,
+        loader: 'csv-loader',
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true
+        }
+      },
     ]
   },
   node: {
