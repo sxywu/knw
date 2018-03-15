1c9d0d05648a21c5cfede92c1182617f4a557d85
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-03-15 15:53:28 +0900

    Add Comets.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <Timeline :data='subtitles' />
+  <div>
+    <Timeline :data='subtitles' />
+    <Comets :data='subtitles'></Comets>
+  </div>
 </template>
 
 <script>
 import Timeline from './components/Timeline';
+import Comets from './components/Comets';
 import subtitles from './assets/gathered_data.csv';
 export default {
   name: 'App',
   components: {
     Timeline,
+    Comets,
   },
   data() {
     return {
---
+++ src/components/Comets.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas></canvas>
+</template>
+
+<script>
+import * as THREE from 'three';
+function anotherFunction() {
+  //
+}
+
+export default {
+  name: 'Comets',
+  props: ['data'],
+  mounted() {
+    this.data
+    this.$el // canvas
+    this.someFunction();
+    anotherFunction();
+  },
+  methods: {
+    someFunction: function() {
+      //
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
